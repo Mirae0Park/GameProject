1a02a9dcafacf38886d226a2373d0b3c92dddffb
parkm <milepark999@gmail.com>
2023-10-10 10:05:32 +0900

    255p

--- src/main/java/com/no3/gameproject/repository/ItemImgRepository.java
+++ src/main/java/com/no3/gameproject/repository/ItemImgRepository.java
@@ -3,5 +3,9 @@ package com.no3.gameproject.repository;
 import com.no3.gameproject.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
